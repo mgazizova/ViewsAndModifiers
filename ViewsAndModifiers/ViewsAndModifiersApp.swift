277bf4d4e1f7302d66ef06315a1c7417f3358700
Mariia Gazizova <mgazizova5@gmail.com>
2024-06-19 20:59:35 +0200

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Мария Газизова on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
